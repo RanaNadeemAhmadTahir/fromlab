1d6d81497d2e36d2b219e0d2a4f866a5dbaefd03
RanaNadeemAhmadTahir <74963539+RanaNadeemAhmadTahir@users.noreply.github.com>
2022-08-09 13:00:19 +0500

    seeding update for default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,13 +1,21 @@
 <?php
 
+use Database\Seeders\RolesTableSeeder;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        $this->call(RolesTableSeeder::class);
 
-            $this->call(RolesTableSeeder::class);
-
+        DB::table('users')->insert([
+            'role' => '1',  //admin
+            'name' => 'rana',
+            'id' => '1',
+            'password' => '$2y$10$6kRFE4e4Bbtxp7JP/gq2h.tcMZ4VFAYeW.eE8HAyrxGkiSNwr/tzK',  //12345678
+            'email' => 'admin@admin.com',
+        ]);
     }
 }
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -15,16 +15,19 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         DB::table('roles')->insert([
+            'role_id' => '1',
             'role_name' => 'Admin',
             'role_slug' => 'admin',
         ]);
         
         DB::table('roles')->insert([
+            'role_id' => '2',
             'role_name' => 'Receptionist',
             'role_slug' => 'receptionist',
         ]);
         
         DB::table('roles')->insert([
+            'role_id' => '3',
             'role_name' => 'Manager',
             'role_slug' => 'manager',
         ]);
